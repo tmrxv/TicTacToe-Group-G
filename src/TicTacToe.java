a2313018b3ebdd832b8fc51db42970296ced9921
Tamir <tamir.bukhshandas@yahoo.de>
2024-07-08 13:53:06 +0200

    implement logic for placing symbols on board (currently just a X)

---
+++ src/TicTacToe.java
@@ -0,0 +1,37 @@
+
+public class TicTacToe {
+    private char[][] board;
+    private char currentPlayer;
+
+    public TicTacToe() {
+        board = new char[3][3];
+        currentPlayer = 'X';
+        initializeBoard();
+    }
+
+    public void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    public void printBoard() {
+        System.out.println("Current Player: " + currentPlayer);
+        System.out.println("  0 1 2");
+        for (int i = 0; i < 3; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void makeMove(int row, int col) {
+        if (row >= 0 && col >= 0 && row < 3 && col < 3 && board[row][col] == '-') {
+            board[row][col] = currentPlayer;
+        }
+    }
+}
